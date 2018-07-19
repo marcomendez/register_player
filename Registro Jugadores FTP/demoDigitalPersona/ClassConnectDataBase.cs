590038a3a4b75052d1b653e9663e9d5fdc637fc9
yacha <yesica.acha@jalasoft.com>
2018-07-19 17:20:55 -0400

    done

--- Registro Jugadores FTP/demoDigitalPersona/ClassConnectDataBase.cs
+++ Registro Jugadores FTP/demoDigitalPersona/ClassConnectDataBase.cs
@@ -23,7 +23,7 @@ namespace demoDigitalPersona
         MySqlTransaction myTrans;
         public ClassConnectDataBase()
         {
-            string aux = "server=sidbol.com;database=sidbol_db;uid=sidbol;pwd=1G3!Nx}$!W]f;";
+            string aux = "server=xxxx.com;database=xxx;uid=xxx;pwd=xxxx;";
 
             myConnection = new MySqlConnection(aux);
         }
@@ -129,7 +129,7 @@ namespace demoDigitalPersona
                 cmd.Parameters["@fingerOne"].Value = fingerOne;
                 cmd.Parameters["@fingerTwo"].Value = fingerTwo;
                 cmd.ExecuteNonQuery();
-                ftp ftpClient = new ftp(@"ftp://sidbol.com", "sidbol", "1G3!Nx}$!W]f");
+                ftp ftpClient = new ftp(@"ftp://sidbol.com", "xxxx", "xxxxx");
 
                 /* Upload a File */
                 ftpClient.upload("public_html/afc/app/webroot/img/players/sm_" + patchphoto + ".jpg", Application.StartupPath + "\\Images\\sm_" + patchphoto + ".jpg");
@@ -194,7 +194,7 @@ namespace demoDigitalPersona
                 cmd.Parameters["@fingerTwo"].Value = fingerTwo;
                 cmd.ExecuteNonQuery();
                 /* Create Object Instance */
-                ftp ftpClient = new ftp(@"ftp://sidbol.com", "sidbol", "1G3!Nx}$!W]f");
+                ftp ftpClient = new ftp(@"ftp://sidbol.com", "xxx", "xxxxx");
 
                 /* Upload a File */
                 ftpClient.upload("public_html/afc/app/webroot/img/players/sm_" + patchphoto +".jpg", Application.StartupPath + "\\Images\\sm_"+ patchphoto+".jpg");
