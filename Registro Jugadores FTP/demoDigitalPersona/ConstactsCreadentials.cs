06aff5a36379f7ddcd1ab771b01cdbf2b8d6735b
yacha <yesica.acha@jalasoft.com>
2018-07-20 14:31:01 -0400

    done

--- Registro Jugadores FTP/demoDigitalPersona/ConstactsCreadentials.cs
+++ Registro Jugadores FTP/demoDigitalPersona/ConstactsCreadentials.cs
@@ -8,12 +8,12 @@ namespace demoDigitalPersona
 {
     public static class ConstactsCreadentials
     {
-  
-        public static string serverName = "sidbol.com";
-        public static string database = "sidbol_db";
 
-        public static string userName = "sidbol";
-        public static string pass = "1G3!Nx}$!W]f";
+        public static string serverName = String.Empty;
+        public static string database = String.Empty;
+
+        public static string userName = String.Empty;
+        public static string pass = String.Empty;
 
     }
 }
